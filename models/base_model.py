a4f25fe68888b824244b1a7d3623b2e0ee9d0f71
CarolinaHV <carolina.hv02@gmail.com>
2021-02-12 02:44:16 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,18 @@ class BaseModel:
     """ Base class """
     def __init__(self, *args, **kwargs):
         """ Initialize public instance attributes """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """ """
