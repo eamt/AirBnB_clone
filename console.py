5e7bdacdbcf901103934ea70ab8d3304fb872dfd
eduard moreno <katananata500@gmail.com>
2021-02-13 13:49:56 +0000

    adding create command

--- console.py
+++ console.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""Define HBNBCommand class"""
 
 import cmd
+import models
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Command prompt class"""
 
     prompt = '(hbnb)'
+    
+    classes= {'BaseModel': BaseModel}
+    
 
     def emptyline(self):
         """Empty options - do Nothing"""
@@ -18,6 +24,26 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, argument):
         """ Defines EOF option"""
         return True
+
+          
+        
+    def do_create(self, argument):
+        """Create a new object in BaseModel
+        Save and print the id """
+
+        if argument:
+            if argument in HBNBCommand.classes.keys():
+                newobject = HBNBCommand.classes[argument]()
+                newobject.save()
+                print(newobject.id)
+
+            if argument not in HBNBCommand.classes.keys():
+                print("**class doesn't exist**")
+
+        elif not argument:
+            print('**class name missing **')
+        
+    """def do_show(self,"""
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
