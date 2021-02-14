fe65b82288587cd4a4183f3ae6d5d0e50b0cba1b
CarolinaHV <carolina.hv02@gmail.com>
2021-02-14 00:35:39 +0000

    Ready task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,7 @@ import os.path
 class FileStorage:
     """File storage """
 
-    __file_path = 'objects.json'
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
@@ -20,24 +20,24 @@ class FileStorage:
     def new(self, obj):
         """Save new object in  __objects"""
 
-        key = obj.__class__.__name__ + "." + str(obj.id)
-        self.__objects[key] = obj
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serialize the content of  __objects to the JSON file"""
 
         json_dict = {}
         """ fill dictionary from __objects"""
-        for key, v in self.__objects.items():
-            json_dict[key] = self.__objects[key].to_dict()
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = FileStorage.__objects[key].to_dict()
             
             with open(self.__file_path, 'w') as f:
                 f.write(json.dumps(json_dict))
                 
- 
     def reload(self):
         """Reload method"""
-       
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as f:
                 json_dict = json.loads(f.read())
+                for key, value in json_dict.items():
+                    FileStorage.__objects[key] = eval(value['__class__'])(**value)
